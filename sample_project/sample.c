2fe81f1219472491729b15dceff1ffcb977458e8
erd <ramaraju.svs@gmail.com>
2020-06-24 21:24:34 -0500

    added add function

--- sample_project/sample.c
+++ sample_project/sample.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+int add ( int a, int b);
 
 void main()
 {
- printf(" hellow world\n");
+ int val1, val2;
+val1= 10;
+val2= val1<<2;
+printf( "a=%d, b=%d, sum =%d\n",val1,val2,add(val1,val2)); 
+ 
+}
+
+int add ( int a, int b)
+{
+  return (a+b);
 }
